fc9cbe7b95f9213404e2ac9d38598fd02b83ba8f
Christopher Castro <y2k2000@gmail.com>
2019-02-14 23:41:04 +0100

    bingo card generator test (wip)

---
+++ tests/Card/CardFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Test\Card;
+
+use Bingo\Card\CardFactory;
+use PHPUnit\Framework\TestCase;
+
+class CardFactoryTest extends TestCase
+{
+
+    /**
+     * ## Scenario:
+     *
+     * - Given I have a Bingo card generator
+     * - When I generate a Bingo card
+     * - Then -> CardUsaTest::*
+     */
+    public function testBuildValidCard()
+    {
+    }
+}
