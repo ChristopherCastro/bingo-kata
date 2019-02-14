df901580565d0182f661e37a9c2b6bd463fccccf
Christopher Castro <y2k2000@gmail.com>
2019-02-14 20:16:00 +0100

    US card skeleton

---
+++ src/Card/CardUsa.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Card;
+
+use Bingo\Card\CardInterface;
+
+class CardUsa implements CardInterface
+{
+
+    public function __construct()
+    {
+    }
+
+    public function getLine($index): array
+    {
+        // TODO: Implement getLine() method.
+    }
+
+    public function setLine($index, array $values = []): void
+    {
+        // TODO: Implement setLine() method.
+    }
+}
